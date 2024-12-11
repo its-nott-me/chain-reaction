ef5734db323a30819ccd3d6e9de8d685b632d3b3
Vivek <22112005vivek@gmail.com>
2024-12-11 14:18:00 +0530

    add meta url and path

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import { instrument } from "@socket.io/admin-ui";
 import { Server } from "socket.io";
 import http from "http";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 import passport, { Passport } from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import { Strategy } from "passport-local";
@@ -39,6 +41,8 @@ const io = new Server(server, {
         credentials: true,
     }
 });
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // const client = createClient();
 
@@ -47,7 +51,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
-
+app.use(express.static(path.join(__dirname, '..', 'chain-reaction', 'build')));
 
 // session setup
 app.use(session({
@@ -454,6 +458,9 @@ io.on("connection", (socket) => {
 
 
 // -- Express Configs --
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'chain-reaction', 'build', 'index.html'));
+});
 app.get("/", (req, res) => {
     res.send("Welcome to chain reaction");
 });
