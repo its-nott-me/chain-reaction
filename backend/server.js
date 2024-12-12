5380c563294b90565aca327a55d2f10dd1fce81c
Vivek <22112005vivek@gmail.com>
2024-12-12 19:41:36 +0530

    cookie debug 5

--- backend/server.js
+++ backend/server.js
@@ -67,7 +67,7 @@ app.use(session({
         maxAge: 24*60*60*1000,
         httpOnly: true,
         sameSite: "none",
-        domain: ".onrender.com" // "localhost" for development
+        domain: "onrender.com" // "localhost" for development
     },
 }));
 // passport setup
