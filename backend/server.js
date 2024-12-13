557f64009dae84c2610a98d45a744b9f628def96
Vivek <22112005vivek@gmail.com>
2024-12-13 16:38:47 +0530

    cookie debug 15

--- backend/server.js
+++ backend/server.js
@@ -62,7 +62,7 @@ app.use(session({
         secure: true,// false for http and treu for https
         maxAge: 24*60*60*1000,
         httpOnly: true,
-        sameSite: "none",
+        sameSite: "lax",
         // domain: ".onrender.com" // "localhost" for development
     },
 }));
